47f25bb847a8087bc7edbc8e1304978fb6af2e5f
Paul Wetter <tellwetter@gmail.com>
2018-03-07 01:15:45 -0600

    Delete CM12_DeleteDDR.ps1

--- CM12_DeleteDDR.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-$SiteCode = "HQ1"
-$MachineName = "DPM01"
-
-$ResourceID = (Get-WmiObject -Class SMS_R_System -Namespace "root\sms\site_$SiteCode" -Filter "Name like `'$MachineName`'").ResourceID
-
-$Resource = [wmi]"\\.\root\sms\site_$($SiteCode):SMS_R_System.resourceID=$ResourceID"
-
-$Resource.psbase.delete()
