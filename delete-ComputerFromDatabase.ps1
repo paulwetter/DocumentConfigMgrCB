9aab89aef1e564ad546ebf1ac5c0a8d191d20447
Paul Wetter <tellwetter@gmail.com>
2018-03-07 01:20:00 -0600

    Delete delete-ComputerFromDatabase.ps1

--- delete-ComputerFromDatabase.ps1
+++
@@ -1,3 +0,0 @@
-﻿$Resource = [wmi](Get-WmiObject -Class SMS_R_SYSTEM -Namespace root\sms\site_pri -ComputerName cm12 -Filter "Name = 'XA002'").__PATH
-
-Remove-WmiObject -InputObject $Resource.__PATH
