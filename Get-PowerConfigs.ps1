8a760c42bd876c99d044a2c46547ee8fdd35a9d5
Paul Wetter <tellwetter@gmail.com>
2018-03-07 01:17:26 -0600

    Delete Get-PowerConfigs.ps1

--- Get-PowerConfigs.ps1
+++
@@ -1,3 +0,0 @@
-﻿[wmi]$collsetting = Get-WmiObject -Class sms_collectionsettings -Namespace root\sms\site_pri -Filter "CollectionID='PRI0001E'"
-$collsetting = $collsetting.__PATH
-$collsetting.PowerConfigs
