28581d1f280ba88a198a9d26d3ddb53e4f7542f1
Paul Wetter <tellwetter@gmail.com>
2018-03-07 01:18:04 -0600

    Delete Run-CMTaskSequence.ps1

--- Run-CMTaskSequence.ps1
+++
@@ -1,8 +0,0 @@
-﻿$UI = New-Object -ComObject "UIResource.UIResourceMgr"
-
-$UI.GetAvailableApplications()
-
-$ProgramID = "*"
-$PackageID = "PRI00061"
-
-$UI.ExecuteProgram($ProgramID,$PackageID,$true)
